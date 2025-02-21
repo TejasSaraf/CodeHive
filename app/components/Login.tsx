a790a6f839dde0626caf2f7227006a034c062649
TejasSaraf <saraftejas12@gmail.com>
2025-02-21 01:08:33 -0600

    Created Next Auth.js

---
+++ app/components/Login.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export function Login() {
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      <div className="flex justify-between">
+        <div>CodeHive</div>
+        <div>
+          {session?.user ? (
+            <button className="m-2 p-2 bg-blue-400" onClick={() => signOut()}>
+              Logout
+            </button>
+          ) : (
+            <button
+              className="m-2 p-2 bg-blue-400"
+              onClick={() => signIn("google")}
+            >
+              Sign In with Google
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
