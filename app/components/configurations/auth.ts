a008ba2a271ab2d404c110bf5120dcf51aa21583
TejasSaraf <saraftejas12@gmail.com>
2025-03-17 23:40:12 -0500

    Implemented Google Auth

--- app/components/configurations/auth.ts
+++ app/components/configurations/auth.ts
@@ -1,24 +1,23 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import Nodemailer from "next-auth/providers/nodemailer"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/app/components/lib/prisma";
+import Nodemailer from "next-auth/providers/nodemailer";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google,
+  providers: [
+    Google,
     Nodemailer({
-        server: {
-          host: process.env.EMAIL_SERVER_HOST,
-          port: process.env.EMAIL_SERVER_PORT,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER,
-            pass: process.env.EMAIL_SERVER_PASSWORD,
-          },
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
         },
-        from: process.env.EMAIL_FROM,
-      }),
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
   adapter: PrismaAdapter(prisma),
-})
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-import { signIn } from "@/app/components/configurations/auth"
  
 export default function Home() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
+    <main>
+      <section className="bg-black">
+      </section>
+    </main>
+    
   )
 } 
